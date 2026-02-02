9cbad7edee7f11ef26386e7cf0c0314adcf094af
Denis <denalfek@gmail.com>
2026-02-02 21:29:47 +0700

    Adds domain entities
    Introduces core domain entities: Applicant, LoanApplication, and LoanDecision.
    These entities define the data structure and relationships for managing
    loan applications and decisions within the system.
    
    Also, includes LoanApplicationStatus enum.

---
+++ src/LoanFlow.Domain/Entities/LoanApplication.cs
@@ -0,0 +1,24 @@
+﻿namespace LoanFlow.Domain.Entities;
+
+public class LoanApplication : BaseEntity<Guid>
+{
+    public Guid ApplicantId { get; set; }
+    public decimal Amount { get; set; }
+    public required string Purpose { get; set; }
+    public LoanApplicationStatus Status { get; set; }
+
+    public DateTime Submitted {  get; set; }
+
+    public virtual required Applicant Applicant { get; set; }
+
+    public virtual LoanDecision? LoanDecision { get; set; }
+}
+
+public enum LoanApplicationStatus
+{
+    Submitted,
+    UnderReview,
+    Approved,
+    Rejected,
+    Disbursed
+}
